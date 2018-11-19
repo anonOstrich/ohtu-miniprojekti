2d083e91384034f4ed69d8337fdbd0c7d7f0d7e4
Jussi Steenari <jussi.steenari@helsinki.fi>
2018-11-19 19:19:47 +0200

    lisätty tallennusoliot

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,16 @@
+
+import bookmarks.BookBookmark;
+import java.util.ArrayList;
+
 public class Main {
 
   public static void main(String[] args){
+      ArrayList<String> tags=new ArrayList<>();
+      tags.add("julma");
+      tags.add( "sydän");
+     
+      BookBookmark bm=new BookBookmark("Seppo", "Juna", "123", new ArrayList<String>() , new ArrayList<String>(), tags);
+      System.out.println(bm);
     System.out.println("Hello tigers of the world!");
   }
 }
